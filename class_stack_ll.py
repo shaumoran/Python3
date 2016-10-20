b4155219fd086fee31e011d9c82880f2e0b2801b
Shauna Moran <shauna.moran34@mail.dcu.ie>
2016-10-20 18:53:11 +0100

    Create class_stack_ll.py

---
+++ class_stack_ll.py
@@ -0,0 +1,15 @@
+#A Stack class for Linked Lists
+
+class Stack:
+    def __init__(self):
+        self.ll = LinkedList()
+
+    def push(self):
+        self.ll.add(item)
+
+    def pop(self):
+        return self.ll.remove()
+
+
+    def is_empty(self):
+        return self.ll.head = None
